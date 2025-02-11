06f71a4751720903d4688f3b4796ff433c88bd6d
NatzNatz <125758574+QQUway@users.noreply.github.com>
2025-02-11 18:08:15 +0700

    State hoisting
    Separated WaterCounter into StatefulCounter and StatelessCounter.

--- app/src/main/java/id/ac/umn/stateinjetpackcompose/MainActivity.kt
+++ app/src/main/java/id/ac/umn/stateinjetpackcompose/MainActivity.kt
@@ -90,3 +90,24 @@ fun WellnessTaskItem(
         }
     }
 }
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+@Composable
+fun StatefulCounter() {
+    var count by remember { mutableStateOf(0) }
+
+    StatelessCounter(count, { count++ })
+    StatelessCounter(count, { count *= 2 })
+}
+
+
--- app/src/main/java/id/ac/umn/stateinjetpackcompose/WellnessScreen.kt
+++ app/src/main/java/id/ac/umn/stateinjetpackcompose/WellnessScreen.kt
@@ -1,7 +1,10 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import id.ac.umn.stateinjetpackcompose.StatefulCounter
+import id.ac.umn.stateinjetpackcompose.WaterCounter
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     WaterCounter(modifier)
+    StatefulCounter(modifier)
 }
